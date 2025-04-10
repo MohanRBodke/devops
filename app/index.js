7cb913d785d63fb3912f73fd45c8c293f985e781
MohanRBodke <119223808+MohanRBodke@users.noreply.github.com>
2025-04-10 21:58:44 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,8 +4,10 @@ const port = 5050;
 
 app.get('/hello', (req, res) => {
     res.send('Hello from DevOps Mini Project!');
+    res.status(200).send('Signup page working!');
 });
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
+
