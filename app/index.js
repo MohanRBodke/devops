a2207d320911fc75253a020dbdff71cd6a83b0cd
MohanRBodke <119223808+MohanRBodke@users.noreply.github.com>
2025-04-10 21:46:15 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5050;
 
 app.get('/hello', (req, res) => {
     res.send('Hello from DevOps Mini Project!');
